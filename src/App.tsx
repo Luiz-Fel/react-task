23c59eed19674c3e820680388b80f19f05adf464
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-03-20 11:59:23 -0300

    Added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.scss';
+import Users from './components/Users';
 
 function App() {
+  const [data, setData] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
+  const [page, setPage] = useState<number>(0);
+  const [disabled, setDisabled] = useState<boolean>(false);
+
+  const previous = () => {
+    if (page === 0) {
+      setDisabled(true);
+    } else {
+      setPage(page - 10);
+    }
+  };
+
+  const next = () => {
+    setPage(page + 10);
+  };
+
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://give-me-users-forever.vercel.app/api/users/${page}/next`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.users);
+        setLoading(false);
+        console.log(data.users)
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, [page]);
+
   return (
     <div className="App">
-     <h1>Hello World!</h1>
+     <header>
+      <h1>React Task</h1>
+      <div className='button-display'>
+        <button onClick={previous} className={` ${disabled || page === 0 ? 'disabled' : ''}`}>
+          Previous
+        </button>
+        <button onClick={next} className={` ${disabled ? 'disabled' : ''}`}>
+          Next
+        </button>
+      </div>
+     </header>
+      
     </div>
   );
 }
